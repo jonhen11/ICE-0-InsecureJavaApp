554852f8dda545ac82b5a179db43d6fcd4b27851
jonhen11 <jonhen11@hotmail.com>
2019-02-06 00:10:18 -0500

    Add files via upload
    Fix Faults right files

--- FileReader.java
+++ FileReader.java
@@ -1,4 +1,4 @@
-// Bailey Miller
+// Jon Hendrickson
 // File reading application that when passed the file name through the args will access the first file
 // With 3 major problems that need fixed
 // 2-5-2019 (4:30 PM)
@@ -22,10 +22,20 @@ public class FileReader
 	// 1
     // https://wiki.sei.cmu.edu/confluence/display/java/FIO16-J.+Canonicalize+path+names+before+validating+them
     // FIO16-J. Canonicalize path names before validating them
+	
     private static String GetFilePath(String[] args)
     {
-        File file = new File("/img/" + args[0]);
-        return file.getPath();
+    	File file = new File("/img/" + args[0]);
+    	if (!isInSecureDir(file)) {
+    	  throw new IllegalArgumentException();
+    	}
+    	String canonicalPath = file.getCanonicalPath();
+    	if (!canonicalPath.equals("/img/java/file1.txt") &&
+    	    !canonicalPath.equals("/img/java/file2.txt")) {
+    	   // Invalid file; handle error
+    	}
+    	 
+    	FileInputStream fis = new FileInputStream(f);
     }
     
     // 2
@@ -33,6 +43,12 @@ public class FileReader
     // FIO02-J. Detect and handle file-related errors (Exceptions)
     private static String GetContents(File f) throws FileNotFoundException, IOException
     {
+    	// add try catch to resolve error handling issue.
+    	try {
     	return FileHelpers.ReadFileContents(f);
+    	} catch (IOException x) {
+    		  System.out.println("Read failed");
+    		  System.out.prinln("Could not read file");
+    		}
     }
 }
